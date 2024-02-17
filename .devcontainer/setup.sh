f67936c1691e871e533bcdb7f4d953d37a7db4fa
Xinghua Pan <23328176+xhorn-pan@users.noreply.github.com>
2024-02-17 22:37:26 +0000

    set and run contracts-node

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,15 +5,19 @@ apt-get install -y \
   git \
   gnupg2 \
   jq \
-  sudo \
-  zsh \
   vim \
   build-essential \
-  openssl
+  openssl\
+  libssl-dev\
+  curl\
+  clang\
+  protobuf-compiler
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
 rustup install nightly
+rustup target add wasm32-unknown-unknown
+rustup target add wasm32-unknown-unknown --toolchain nightly
 rustup component add rustfmt
 rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
@@ -21,6 +25,8 @@ rustup component add clippy --toolchain nightly
 
 cargo install cargo-expand
 cargo install cargo-edit
+cargo install cargo-contract
+cargo install contracts-node
 
 ## setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
