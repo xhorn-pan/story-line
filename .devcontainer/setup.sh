ba50d9a13c034da057e17ba93a2f722f8fc3fc2d
Xinghua Pan <23328176+xhorn-pan@users.noreply.github.com>
2024-02-26 15:47:27 +0000

    delete zsh setup, for now

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -32,9 +32,3 @@ cargo install cargo-edit
 cargo install cargo-contract
 cargo install contracts-node
 
-## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
