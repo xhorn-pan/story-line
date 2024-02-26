370318d56d83b716a24b25de00c8bcd7f4a8efe4
Xinghua Pan <23328176+xhorn-pan@users.noreply.github.com>
2024-02-26 16:30:34 +0000

    ...

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,23 +12,3 @@ apt-get install -y \
   curl\
   clang\
   protobuf-compiler
-
-#install nodejs
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && apt-get install -y nodejs
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-
-## Install rustup and common components
-curl https://sh.rustup.rs -sSf | sh -s -- -y 
-rustup install nightly
-rustup target add wasm32-unknown-unknown
-rustup target add wasm32-unknown-unknown --toolchain nightly
-rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
-rustup component add clippy 
-rustup component add clippy --toolchain nightly
-
-cargo install cargo-expand
-cargo install cargo-edit
-cargo install cargo-contract
-cargo install contracts-node
-
